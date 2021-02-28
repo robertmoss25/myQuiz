4be0dfde9293f51795325f4542e9df257496a290
robertmoss25 <42865029+robertmoss25@users.noreply.github.com>
2021-02-28 09:57:35 -0500

    Updated with new files

---
+++ end.js
@@ -0,0 +1,30 @@
+const username = document.getElementById('username');
+const finalScore = document.getElementById('finalScore');
+const saveScoreBtn = document.getElementById('saveScoreBtn');
+const mostRecentScore = localStorage.getItem('mostRecentScore');
+
+finalScore.innerText = mostRecentScore;
+
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+const MAX_HIGH_SCORES = 5;
+
+username.addEventListener('keyup', () => {
+    saveScoreBtn.disabled = !username.value;
+})
+
+saveHighScore = e => {
+    console.log("Clicked the save button!");
+    e.preventDefault();
+
+    const score = {
+        score: mostRecentScore,
+        name: username.value
+    };
+    highScores.push(score);
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(MAX_HIGH_SCORES);
+
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    window.location.assign("/");
+};
