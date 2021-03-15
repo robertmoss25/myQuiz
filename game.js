9089c2425808d2dbcf96fc40e601397bd52685bd
robertmoss25 <42865029+robertmoss25@users.noreply.github.com>
2021-03-15 18:01:17 -0400

    Updated

--- game.js
+++ game.js
@@ -5,6 +5,7 @@ const scoreText = document.getElementById('score');
 const progressBarFull = document.getElementById('progressBarFull'); 
 const loader = document.getElementById('loader'); 
 const game = document.getElementById('game');
+const questionlistchosen = document.getElementById('questionlistchosen');
 
 let currentQuestion = {};
 let acceptingAnswers = true;
@@ -14,7 +15,24 @@ let availableQuestions = [];
 
 let questions = [];
 
-fetch("questions.json")
+let answers = [];
+
+function getParameterByName(name, url = window.location.href) {
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+let selectedGame = getParameterByName('myGame');
+let selectedTitle = getParameterByName('myTitle');
+console.log(selectedGame);
+questionlistchosen.innerText = selectedTitle;
+
+////fetch("questions.json")
+fetch(selectedGame)
     .then(res => {
         return res.json();
 }).then(loadedQuestions => {
@@ -66,7 +84,7 @@ startGame = () => {
 getNewQuestion = () => {
     if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
         localStorage.setItem("mostRecentScore", score);
-        return window.location.assign("/end.html");
+        return window.location.assign("./end.html");
     }
     questionCounter++;
     progressText.innerHTML = `Question ${questionCounter}/${MAX_QUESTIONS}`;
@@ -74,14 +92,42 @@ getNewQuestion = () => {
     progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
-    question.innerText = currentQuestion.question;
+    let value = parseInt(currentQuestion.multi);
+    if (value > 1)
+        question.innerText = currentQuestion.question + " ( Pick " + value + ")";
+    else
+        question.innerText = currentQuestion.question;
 
     choices.forEach( choice => {
         const number = choice.dataset['number'];
-        choice.innerText = currentQuestion["choice" + number];
+        let questionID = null; 
+        if (number == '1')
+            questionID = document.getElementById('One');
+        else if (number == '2')
+            questionID = document.getElementById('Two');
+        else if (number == '3')
+            questionID = document.getElementById('Three');
+        else if (number == '4')
+            questionID = document.getElementById('Four'); 
+        else if (number == '5')
+            questionID = document.getElementById('Five');
+        else if (number == '6')
+            questionID = document.getElementById('Six'); 
+        else if (number == '7')
+            questionID = document.getElementById('Seven'); 
+        if (currentQuestion["choice" + number] == undefined || currentQuestion["choice" + number] == 'Filler')
+        {   
+            questionID.setAttribute("style", "display: none;");
+        }
+        else
+        {
+            choice.innerText = currentQuestion["choice" + number];
+            questionID.removeAttribute("style", "display: none;");
+        }
     });
 
     availableQuestions.splice(questionIndex,1);
+    answers.length = 0;
 
     acceptingAnswers = true;
 };
@@ -90,27 +136,101 @@ choices.forEach(choice => {
     choice.addEventListener("click", e => {
         if (!acceptingAnswers)
             return;
-        acceptingAnswers = false;
-        const selectedChoice = e.target;
-        const selectedAnswer = selectedChoice.dataset["number"];
+        console.log("multi is " + currentQuestion.multi);
+        if (currentQuestion.multi != 0)
+        {
+            answers.push(e.target);
+            if (answers.length == currentQuestion.multi)
+            {
+                let correctAnswer = 0;
+                let answerArr = currentQuestion.answer.split(',');
+                for (let index = 0; index < answerArr.length; index++) 
+                {
+                    const element = answerArr[index];
+                    for (let index2 = 0; index2 < answers.length; index2++) 
+                    {
+                        const element2 = answers[index2];
+                        const selectedAnswer = element2.dataset["number"];
+                        if (element == selectedAnswer)
+                        {
+                            console.log(element + " " + selectedAnswer);
+                            correctAnswer++;
+                            break;
+                        }
+                    }   
+                }
 
-        let classToApply = 'incorrect';
-        if (selectedAnswer == currentQuestion.answer) {
-            classToApply = 'correct';
+                if (correctAnswer == answers.length)
+                {
+                    for (let index = 0; index < answers.length; index++)
+                    {
+                        classToApply = 'correct';
+                        answers[index].parentElement.classList.remove('chosen');
+                        answers[index].parentElement.classList.add(classToApply);
+                    }
+                    incrementScore(classToApply == "correct" ? CORRECT_BONUS : 0);
+
+                    setTimeout( () => {
+                        for (let index = 0; index < answers.length; index++)
+                        {
+                            answers[index].parentElement.classList.remove(classToApply);
+                        }                        
+                        getNewQuestion();
+                    }, 1000);
+                }
+                else
+                {
+                    for (let index = 0; index < answers.length; index++)
+                    {
+                        classToApply = 'incorrect';
+                        answers[index].parentElement.classList.add(classToApply);
+                        answers[index].parentElement.classList.remove('chosen');
+                    }
+                    incrementScore(classToApply == "correct" ? CORRECT_BONUS : 0);
+
+                    setTimeout( () => {
+                        for (let index = 0; index < answers.length; index++)
+                        {
+                            answers[index].parentElement.classList.remove(classToApply);
+                        }                        
+                        getNewQuestion();
+                    }, 1000);
+                }
+            }
+            else
+            {
+                const selectedChoice = e.target;
+                const selectedAnswer = selectedChoice.dataset["number"];
+
+                classToApply = 'chosen';
+                selectedChoice.parentElement.classList.add(classToApply);
+            }
         }
-        incrementScore(classToApply == "correct" ? CORRECT_BONUS : 0);
-        selectedChoice.parentElement.classList.add(classToApply);
+        else
+        {
+            acceptingAnswers = false;
+            const selectedChoice = e.target;
+            const selectedAnswer = selectedChoice.dataset["number"];
 
-        setTimeout( () => {
-            selectedChoice.parentElement.classList.remove(classToApply);
-            getNewQuestion();
-        }, 1000);
+            let classToApply = 'incorrect';
+            if (selectedAnswer == currentQuestion.answer) {
+                classToApply = 'correct';
+            }
+            incrementScore(classToApply == "correct" ? CORRECT_BONUS : 0);
+            selectedChoice.parentElement.classList.add(classToApply);
+
+            setTimeout( () => {
+                selectedChoice.parentElement.classList.remove(classToApply);
+                getNewQuestion();
+            }, 1000);
+        }
     })
 });
 
 incrementScore = num => {
     score += num;
-    scoreText.innerText = score;
+    let currentPercent = ((score / 10) / questionCounter) * 100;
+    scoreText.innerText = score + " (" + currentPercent.toFixed(2) + "%)";
 };
 
 // startGame();
